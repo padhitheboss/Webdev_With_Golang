b5d635cb46e330d1b1f8bc65c4535f980c7311ee
padhitheboss <padhithebossno.1@gmail.com>
2023-01-15 14:29:24 +0530

    Added 1.Basic Webserver

---
+++ 1.Webserver/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "Hello! There")
+}
+func formhandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formhandler)
+	http.HandleFunc("/hello", hellohandler)
+
+	fmt.Printf("Starting Webserver on port 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
